64b6d7dc9ee8123cebdfee174a04af464f5ab000
Andrew Wiedenmann <34554935+andy7809@users.noreply.github.com>
2023-10-19 16:33:20 -0500

    use reportf instead of local logging

--- ssa_analysis.go
+++ ssa_analysis.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/buildssa"
@@ -19,29 +17,19 @@ var EmbedAnalysis = &analysis.Analyzer{
 var SSAAnalysis = buildssa.Analyzer
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	// for key, value := range pass.ResultOf {
-	// 	fmt.Printf("%s: %d\n", key, value)
-	// }
-
 	ssaProg := pass.ResultOf[SSAAnalysis].(*buildssa.SSA)
 	for _, fn := range ssaProg.SrcFuncs {
 		for _, b := range fn.Blocks {
 			for _, i := range b.Instrs {
 				if p, ok := i.(*ssa.Phi); ok {
-					// fmt.Printf("[%-20T] %s = %s\n", i, p.Name(), i)
 					values := make(map[string]bool)
 					for _, op := range p.Edges {
-						// fmt.Printf("%+v\n", op)
 						if binop, ok := op.(*ssa.BinOp); ok {
 							if values[binop.String()] {
-								fmt.Printf("phi type: [%-15T]\n", binop)
-								fmt.Printf("X: %s\n", binop.X.Name())
-								fmt.Printf("Y: %s\n", binop.Y.Name())
+								pass.Reportf(binop.Pos(), "found equivalent SSA values")
 							}
 							values[op.String()] = true
-
 						}
-						// fmt.Printf("%v\n", values)
 					}
 				}
 			}
